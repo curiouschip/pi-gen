01457b1689784dd297f45f2a64ad5ee7094bdf14
Jason Frame <jwf@jasonframe.co.uk>
2020-12-01 14:22:42 +0000

    remove dt-blob.bin for now as it's broken; put config.txt in correct place

--- pip-after/00-boot/00-run.sh
+++ pip-after/00-boot/00-run.sh
@@ -14,6 +14,6 @@ make
 popd
 
 install -m 644 $PIP_BOOT_DIR/overlays/*.dtbo "${ROOTFS_DIR}/boot/overlays/"
-install -m 644 $PIP_BOOT_DIR/config.txt "${ROOTFS_DIR}/boot/config_pip.txt"
-install -m 644 $PIP_BOOT_DIR/dt-blob.bin "${ROOTFS_DIR}/boot/dt-blob.bin"
+install -m 644 $PIP_BOOT_DIR/config.txt "${ROOTFS_DIR}/boot/config.txt"
+#install -m 644 $PIP_BOOT_DIR/dt-blob.bin "${ROOTFS_DIR}/boot/dt-blob.bin"
 
