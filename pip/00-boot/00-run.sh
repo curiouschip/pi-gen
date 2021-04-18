e6d0690b934a0c56d0b38a3708b29962a91110cc
Jason Frame <jwf@jasonframe.co.uk>
2021-04-18 19:27:07 +0100

    Use correct cmdline.txt on debug/release builds

--- pip/00-boot/00-run.sh
+++ pip/00-boot/00-run.sh
@@ -17,10 +17,10 @@ install -m 644 $PIP_BOOT_DIR/config.txt "${ROOTFS_DIR}/boot/config.txt"
 install -m 644 $PIP_BOOT_DIR/dt-blob.bin "${ROOTFS_DIR}/boot/dt-blob.bin"
 
 if [ "$DEBUG" = "1" ]; then
-	install -m 644 $PIP_BOOT_DIR/cmdline.txt "${ROOTFS_DIR}/boot/cmdline.txt"
+	install -m 644 $PIP_BOOT_DIR/cmdline-debug.txt "${ROOTFS_DIR}/boot/cmdline.txt"
 	sudo install -m 600 -o root -g root files/wpa_supplicant.conf $ROOTFS_DIR/etc/wpa_supplicant/wpa_supplicant.conf
 else
-	install -m 644 $PIP_BOOT_DIR/cmdline-debug.txt "${ROOTFS_DIR}/boot/cmdline.txt"
+	install -m 644 $PIP_BOOT_DIR/cmdline.txt "${ROOTFS_DIR}/boot/cmdline.txt"
 fi
 
 on_chroot << EOF
