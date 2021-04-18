047b9b7450d086552b42c4d02966cc2c2665b3ce
Jason Frame <jwf@jasonframe.co.uk>
2021-04-18 11:29:03 +0100

    wip

--- pip-after/00-boot/00-run.sh
+++ pip-after/00-boot/00-run.sh
@@ -15,5 +15,10 @@ popd
 
 install -m 644 $PIP_BOOT_DIR/overlays/*.dtbo "${ROOTFS_DIR}/boot/overlays/"
 install -m 644 $PIP_BOOT_DIR/config.txt "${ROOTFS_DIR}/boot/config.txt"
+install -m 644 $PIP_BOOT_DIR/cmdline.txt "${ROOTFS_DIR}/boot/cmdline.txt"
 install -m 644 $PIP_BOOT_DIR/dt-blob.bin "${ROOTFS_DIR}/boot/dt-blob.bin"
+
+on_chroot << EOF
+apt-get update
+EOF
 
